c5c62ba6be71a81e219339e1e670f42cc6f96e4f
ecourse <smkang@codenuri.co.kr>
2023-10-06 15:42:26 +0900

    Add files via upload

--- SECTION7_UTILITY/BIND_AND_FUNCTION/std_function1.cpp
+++ SECTION7_UTILITY/BIND_AND_FUNCTION/std_function1.cpp
@@ -10,13 +10,11 @@ int main()
 {
 	void(*f)(int); 
 
-	f = &f1;	// ok 
-	f(10); 		// f1(10)
-
-	f = &f2;
-	f = &Window::set_width;
+	f = &f1;	// ok
+	f = &f2;	// error.
+	f = &Window::set_width;	// error
 
 	int n = 0;
-	f = [ ](int a) { };
-	f = [n](int a) { };
+	f = [ ](int a) { };	// ok
+	f = [n](int a) { };	// error 
 }
