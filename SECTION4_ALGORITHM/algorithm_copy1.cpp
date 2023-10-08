0a932db0f718b7dd43afb125c8295f5166ca5d0f
ecourse <smkang@codenuri.co.kr>
2023-10-08 21:22:27 +0900

    Add files via upload

--- SECTION4_ALGORITHM/algorithm_copy1.cpp
+++ SECTION4_ALGORITHM/algorithm_copy1.cpp
@@ -1,23 +1,16 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include "cmc.h"
 
 int main()
 {
-	std::vector v1{1, 3, 6, 3, 5};
-	std::vector v2{0, 0, 0, 0, 0};
+	std::vector v1{1, 2, 3, 1, 2, 3, 1, 2, 3, 1};
+	std::vector v2{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-	namespace rgs = std::ranges;
+//	auto ret = std::ranges::remove(v1, 3);
+	auto ret = std::ranges::remove_copy(v1, v2.begin(), 3);
 
-	auto ret1 = rgs::remove(v1, 3);
-
-//	auto ret2 = rgs::remove_copy(v1, v2.begin(), 3);
-
-//	auto ret3 = rgs::remove_if(v1, 
-//					[](int n){ return n % 3 == 0;});
-
-//	auto ret4 = rgs::remove_copy_if(v1, v2.begin(), 
-//					[](int n){ return n % 3 == 0;});
 	show(v1);
 	show(v2);
 }
+
